4a0fa038ed9df72d221206f9037f90b0b5d3c7b6
lucimarNeves <lucimar.neves@gmail.com>
2024-11-11 18:52:49 -0300

    Project Created

---
+++ src/test/java/com/aula/devsuperior/exemplo_um/ExemploUmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula.devsuperior.exemplo_um;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploUmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
