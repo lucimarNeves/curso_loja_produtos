1240a44776de10175ec51ae9e64cbdc6834e99c2
lucimarNeves <lucimar.neves@gmail.com>
2024-11-17 21:09:21 -0300

    Homolog ok

--- src/main/java/com/aula/devsuperior/exemplo_um/DTO/GameDTO.java
+++ src/main/java/com/aula/devsuperior/exemplo_um/DTO/GameDTO.java
@@ -22,7 +22,7 @@ public class GameDTO {
       public GameDTO(GameMinProjection entity) {
         id = entity.getId();
         title = entity.getTitle();
-        year = entity.getYear();
+        year = entity.getGameYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
     }
